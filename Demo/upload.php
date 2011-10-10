7355bf3f01d2afa0f9cb5384abde62066e1f61c5
SamGoody <siteroller@gmail.com>
2011-10-10 10:52:57 +0200

    Corrected path spelling in Demo for case-sensitive servers.

--- Demo/upload.php
+++ Demo/upload.php
@@ -1,7 +1,7 @@
 <?php
 
 // Load mooupload class
-require('..'.DIRECTORY_SEPARATOR.'source'.DIRECTORY_SEPARATOR.'mooupload.php');
+require('..'.DIRECTORY_SEPARATOR.'Source'.DIRECTORY_SEPARATOR.'mooupload.php');
 
 // Upload file to tmp directory
 Mooupload::upload(dirname(__FILE__).DIRECTORY_SEPARATOR.'tmp'.DIRECTORY_SEPARATOR);
